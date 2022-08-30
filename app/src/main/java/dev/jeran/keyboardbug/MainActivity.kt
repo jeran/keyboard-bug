c3237adc867b74c6fb730adde9073dc4e4c2d5df
jeran <jerantfox@gmail.com>
2022-08-30 16:29:44 -0400

    Add modal bottom sheet with text field
    As is, the animation of opening the keyboard is pretty rough. I'm not sure why, but we need to call `WindowCompat.setDecorFitsSystemWindows(window, false)` for it to function properly. I would have thought this was only necessary if using transparent system bars and systemBarsPadding().

--- app/src/main/java/dev/jeran/keyboardbug/MainActivity.kt
+++ app/src/main/java/dev/jeran/keyboardbug/MainActivity.kt
@@ -3,41 +3,58 @@ package dev.jeran.keyboardbug
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.material.Button
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextField
+import androidx.compose.material.rememberModalBottomSheetState
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import dev.jeran.keyboardbug.ui.theme.KeyboardBugTheme
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterialApi::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            KeyboardBugTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+            val modalBottomSheetState = rememberModalBottomSheetState(
+                initialValue = ModalBottomSheetValue.Hidden,
+                skipHalfExpanded = true,
+            )
+            val scope = rememberCoroutineScope()
+            ModalBottomSheetLayout(
+                sheetContent = {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .imePadding(),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        TextField(
+                            value = "",
+                            onValueChange = { },
+                        )
+                    }
+                },
+                sheetState = modalBottomSheetState,
+            ) {
+                Box(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    contentAlignment = Alignment.Center,
                 ) {
-                    Greeting("Android")
+                    Button(
+                        onClick = { scope.launch { modalBottomSheetState.show() } },
+                    ) {
+                        Text(text = "Open Sheet")
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    KeyboardBugTheme {
-        Greeting("Android")
     }
 }
